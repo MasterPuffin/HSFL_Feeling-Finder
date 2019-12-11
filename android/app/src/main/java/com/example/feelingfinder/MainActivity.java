d6dfe5fadf00875ce6d1586786b2760586590319
MasterPuffin <johannes.bluhm@outlook.com>
2019-12-11 15:53:35 +0100

    added js func

--- android/app/src/main/java/com/example/feelingfinder/MainActivity.java
+++ android/app/src/main/java/com/example/feelingfinder/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.feelingfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("https://masterpuffin.github.io/Feeling-Finder/home.html");
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
     }
 }
