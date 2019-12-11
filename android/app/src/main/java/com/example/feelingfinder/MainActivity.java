3f1bb0cbdfdf92adbe2d7f5fa5cd51d410b018cd
MasterPuffin <johannes.bluhm@outlook.com>
2019-12-11 15:14:54 +0100

    added wrapper app

---
+++ android/app/src/main/java/com/example/feelingfinder/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.feelingfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://masterpuffin.github.io/Feeling-Finder/home.html");
+    }
+}
